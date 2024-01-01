fffa3a162682ca8aca5f781fa705551f8c549e72
DESKTOP-0MNRU9B\Kenneth <knnthcrtl.kc@gmail.com>
2024-01-01 23:19:44 +0800

    setup boilerplate tailwind and rn paper

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
+import * as React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [],
+  content: ['./App.{js,jsx,ts,tsx}', './<custom-folder>/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
