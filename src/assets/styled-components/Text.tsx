e1a22270d5500e5eda9d374df3e7dc8640d6fcd4
DESKTOP-0MNRU9B\Kenneth <knnthcrtl.kc@gmail.com>
2024-01-03 01:19:20 +0800

    setup component and svg

---
+++ src/assets/styled-components/Text.tsx
@@ -0,0 +1,6 @@
+import {styled} from 'nativewind';
+import {Text as StyledText} from 'react-native-paper';
+
+const Text = styled(StyledText);
+
+export default Text;
---
+++ src/assets/styled-components/View.tsx
@@ -0,0 +1,6 @@
+import {styled} from 'nativewind';
+import {View as StyledView} from 'react-native';
+
+const View = styled(StyledView);
+
+export default View;
